f07b8684a381461df79340f6de1d9798f88e1abc
Krishna Valli Nune <krishnavallivalli541@gmail.com>
2025-04-03 11:17:34 +0530

    krishna

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TaskProvider } from './context/TaskContext';
 import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import TaskList from './components/TaskLists';
 import { CssBaseline, Container, Typography } from '@mui/material';
 
 function App() {
